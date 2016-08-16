89b48dfda0fab3032b10bb6fd244fe6f407a368d
Bruce Prewit <bprewit@dyess.tenstar.lan>
2016-08-16 15:40:37 -0500

    Solution to yodle triangle problem.

--- triangle-sum.cpp
+++ triangle-sum.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 using namespace std;
 typedef vector<int> ROW_T;
-
-void dumpit(vector<ROW_T> t)
-{
-	int i;
-	for(i = 0; i < t.size(); ++i)
-	{
-		cout << i << ":" << t[i].size() << endl;
-	}
-}
 
 int main(int argc, char *argv[])
 {
@@ -21,10 +13,11 @@ int main(int argc, char *argv[])
 	for(int r = 0; getline(cin, s); ++r)
 	{
 		ROW_T row;
-		for(int j = 0; j <= r; ++j)
+		istringstream iss(s);
+		while(!iss.eof())
 		{
 			int n;
-			cin >> n;
+			iss >> n;
 			row.push_back(n);
 		}
 		triangle.push_back(row);
@@ -34,7 +27,7 @@ int main(int argc, char *argv[])
 	for(int row = (rows - 2); row >= 0; --row)
 	{
 		int cols = triangle[row].size();
-		cerr << "row " << row << " cols " << cols << endl;
+		
 		for(int col = 0; col < cols; ++col)
 		{
 			triangle[row][col] = triangle[row][col] + max(triangle[row+1][col], triangle[row+1][col+1]);
